fdc3476c7dbbe8a75d921b29ee0758d85d08fbd4
afly <15732136803@163.com>
2018-11-29 15:51:37 +0800

    删除了些文件

--- client/push_stream.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-cmd = 'ffmpeg -f dshow -i video="EasyCamera":audio="麦克风 (Realtek High Definition Audio)" -vcodec libx264  -tune:v zerolatency -f flv rtmp://59.110.140.100:1935/live/123'
-p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-p.wait()
-
